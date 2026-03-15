99840cecb9cffbc231f95e01ac8e3138f7f6e645
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-03-15 14:12:27 -0400

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,14 +1,21 @@
 import mongoose from "mongoose";
 
 const orderSchema = new mongoose.Schema(
-  {
-    buyerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // virtual buyer
-    customerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }, // real customer
-    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-    status: { type: String, default: "pending" }, // pending, picked, delivery, completed
-    price: { type: Number, required: true },
-  },
-  { timestamps: true }
+{
+buyerId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
+customerId:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
+productId:{type:mongoose.Schema.Types.ObjectId,ref:"Product",required:true},
+
+status:{
+type:String,
+enum:["pending","picked","delivery","completed"],
+default:"pending"
+},
+
+price:{type:Number,required:true}, // sell price
+buyPrice:{type:Number,required:true} // company price
+},
+{timestamps:true}
 );
 
-export default mongoose.model("Order", orderSchema);
+export default mongoose.model("Order",orderSchema);
