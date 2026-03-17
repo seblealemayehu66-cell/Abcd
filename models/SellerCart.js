8d3a214ee7b105503445ab891aa28e9aecc78d08
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-03-17 13:33:50 -0400

    Update SellerCart.js

--- models/SellerCart.js
+++ models/SellerCart.js
@@ -1,23 +1,30 @@
 import mongoose from "mongoose";
 
 const sellerCartSchema = new mongoose.Schema(
-{
-  sellerId:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:"User",
-    required:true
+  {
+    sellerId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true
+    },
+
+    price: {
+      type: Number,
+      required: true
+    },
+
+    quantity: {
+      type: Number,
+      default: 1
+    }
   },
-  productId:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref:"Product"
-  },
-  price:Number,
-  stock:Number,
-  quantity:{
-    type:Number,
-    default:1
-  }
-},{timestamps:true}
+  { timestamps: true }
 );
 
-export default mongoose.model("SellerCart",sellerCartSchema);
+export default mongoose.model("SellerCart", sellerCartSchema);
