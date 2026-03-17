7a2aac16c7ecc642ea8aa577a71749a8bd088cea
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-03-17 08:34:17 -0400

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,7 +4,7 @@ import {
   updateProfile,
   updateShop,
 } from "../controllers/userController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js";
+import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
