0812be5e8cdafb855339508e4038a6098e594033
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-03-17 14:13:29 -0400

    Update SellerCart.js

--- models/SellerCart.js
+++ models/SellerCart.js
@@ -2,27 +2,11 @@ import mongoose from "mongoose";
 
 const sellerCartSchema = new mongoose.Schema(
   {
-    sellerId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true
-    },
-
-    productId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Product",
-      required: true
-    },
-
-    price: {
-      type: Number,
-      required: true
-    },
-
-    quantity: {
-      type: Number,
-      default: 1
-    }
+    sellerId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, default: 1 },
+    stock: { type: Number, required: true }, // snapshot of product stock at time of adding
   },
   { timestamps: true }
 );
