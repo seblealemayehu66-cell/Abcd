39ef77f20aca09bde96ef7178865a3615c8ecc91
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-03-19 15:28:42 -0400

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 const cartSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   items: [
     {
-      product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
       quantity: { type: Number, default: 1 },
-      size: String,
-      color: String,
-    },
-  ],
+      size: { type: String },
+      color: { type: String }
+    }
+  ]
 }, { timestamps: true });
 
 export default mongoose.model("Cart", cartSchema);
