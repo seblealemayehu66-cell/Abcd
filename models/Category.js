692f004827987d66fc87fc19f8543fe6eed16e01
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-03-20 16:51:13 -0400

    Update Category.js

--- models/Category.js
+++ models/Category.js
@@ -1,13 +1,17 @@
+// models/Category.js
 import mongoose from "mongoose";
 
-const categorySchema = new mongoose.Schema({
+const subCategorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+});
 
+const categorySchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    unique: true
-  }
-
+    unique: true,
+  },
+  subCategories: [subCategorySchema], // array of subcategories
 });
 
 export default mongoose.model("Category", categorySchema);
