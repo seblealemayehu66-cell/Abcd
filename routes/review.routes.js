6640b06a680f4351a2ace2151e6e335b778a836f
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-03-25 18:48:57 -0400

    Create review.routes.js

---
+++ routes/review.routes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { addReview } from "../controllers/review.controller.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", authMiddleware, addReview);
+
+export default router;
