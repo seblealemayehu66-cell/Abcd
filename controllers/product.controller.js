35b7d622dd13dc8e4464e84722fc9f4bb1f879b9
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-05-23 08:30:44 -0400

    Update product.controller.js

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -572,48 +572,4 @@ export const getSellerProducts = async (req, res) => {
     console.log(error);
     res.status(500).json({ message: "Server error" });
   }
-};.......................and product.routes.js import express from "express";
-import multer from "multer";
-
-import {
-  addProduct,
-  getProducts,
-  getProductsByCategory,
-  publishCart ,
-  getSellerProducts,
-  getSingleProduct,
-} from "../controllers/product.controller.js";
-
-import upload from "../middleware/upload.js";
-import authMiddleware from "../middleware/authMiddleware.js";
-import { bulkImportProducts } from "../controllers/product.controller.js";
-const router = express.Router();
-
-const excelUpload = multer({
-  dest: "uploads/excel/"
-});
-
-// ✅ ADD PRODUCT (MULTIPLE IMAGES)
-router.post("/", upload.array("images", 5), addProduct); 
-
-router.get("/", getProducts);
-router.get("/:id", getSingleProduct);
-  
-
-router.get("/category/:categoryId", getProductsByCategory);
-
-
-// SELLER PUBLISH
-router.post("/publish", authMiddleware, publishCart );
-
-
-// SELLER STORE PRODUCTS
-router.get("/seller", authMiddleware, getSellerProducts);
-router.post(
-  "/bulk-import",
-  excelUpload.single("file"),
-  bulkImportProducts
-);
-
-
-export default router;
+};
