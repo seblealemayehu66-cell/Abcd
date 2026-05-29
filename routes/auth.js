17a5a08ba931a481e61aa1e027eedb94ed78de54
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-05-29 16:40:07 -0400

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -63,5 +63,44 @@ router.post("/register", async (req, res) => {
     res.status(500).json({ msg: "Server error" });
   }
 });
+router.get("/me", async (req, res) => {
+
+  try {
+
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "No token"
+      });
+    }
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    const user = await User.findById(decoded.id)
+      .select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found"
+      });
+    }
+
+    res.json(user);
+
+  } catch (error) {
+
+    console.log(error);
+
+    res.status(401).json({
+      message: "Invalid token"
+    });
+
+  }
+
+});
 
 module.exports = router;
