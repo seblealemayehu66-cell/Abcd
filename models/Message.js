c030f08b89dc4ae62cc622e02e4c5810df62fb4a
seblealemayehu66-cell <seblealemayehu66@gmail.com>
2026-06-05 16:07:32 -0400

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,22 +1,27 @@
 import mongoose from "mongoose";
 
 const messageSchema = new mongoose.Schema(
-{
-    chatId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Chat"
+  {
+    sender: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
 
-    sender:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+    receiver: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
 
-    text:{
-        type:String
-    }
-},
-{ timestamps:true }
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 export default mongoose.model("Message", messageSchema);
